14b19aecdc7629b332bc778de24c734ab80dceec
Ramon <ramondiiaas@hotmail.com>
2023-07-18 16:18:46 -0300

    add: cactus obstacle and adjusts some conventions

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,12 +1,14 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
 
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
-DUCK_VEL = 8.5
 
-class Dinosaur:
+
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -14,7 +16,6 @@ class Dinosaur:
         self.dino_rect.y = Y_POS
         self.step_index = 0
         self.jump_vel = JUMP_VEL
-        self.duck_vel = DUCK_VEL
         self.dino_run = True
         self.dino_jump = False
         self.dino_duck = False
@@ -31,13 +32,13 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif  user_input[pygame.K_DOWN] and not self.dino_duck:
+        elif  user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
             self.dino_jump = False
         elif not self.dino_duck and not self.dino_jump:
-            self.dino_duck = False
             self.dino_run = True
+            self.dino_duck = False
             self.dino_jump = False
 
         if self.step_index >= 10:
